6cb203d5466eaee2e4e77dbac0c9eea5feb74a7d
Artem Artemev <arteitip@gmail.com>
2020-11-22 13:18:44 +0500

    refactor: remove shared material module

--- src/app/pages/review/review.module.ts
+++ src/app/pages/review/review.module.ts
@@ -3,20 +3,48 @@ import { CommonModule } from '@angular/common';
 
 import { ReviewRoutingModule } from './review-routing.module';
 import { SharedModule } from '@app/shared/shared.module';
-import { SharedMaterialModule } from '@app/shared/shared-material/shared-material.module';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 import { SingleReviewComponent } from './components/single-review/single-review.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ReviewsComponent, SingleReviewComponent],
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     ReviewRoutingModule,
     SharedModule,
-    SharedMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
+    MatRippleModule,
   ],
 })
 export class ReviewModule {}
